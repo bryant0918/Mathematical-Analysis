c71bf1223bea89e67bc2de68da13b4ccac74419c
bryant <bmcarth4@byu.edu>
2021-11-02 10:56:56 -0600

    For soft grade

--- ImageSegmentation/image_segmentation.py
+++ ImageSegmentation/image_segmentation.py
@@ -1,12 +1,15 @@
 # image_segmentation.py
 """Volume 1: Image Segmentation.
-<Name>
-<Class>
-<Date>
+Bryant McArthur
+November 2
+Sec 002
 """
 
 import numpy as np
-
+from scipy import linalg as la
+from scipy import sparse as sp
+from imageio import imread
+from matplotlib import pyplot as plt
 
 # Problem 1
 def laplacian(A):
@@ -18,7 +21,17 @@ def laplacian(A):
     Returns:
         L ((N,N) ndarray): The Laplacian matrix of G.
     """
-    raise NotImplementedError("Problem 1 Incomplete")
+    m,n = np.shape(A)
+    rowsums = []
+    
+    for i in range(m):
+        rowsums.append(sum(A[i,j] for j in range(m)))
+        
+    D = np.diag(rowsums)
+    L = D - A
+    
+    return L
+        
 
 
 # Problem 2
@@ -35,7 +48,22 @@ def connectivity(A, tol=1e-8):
         (int): The number of connected components in G.
         (float): the algebraic connectivity of G.
     """
-    raise NotImplementedError("Problem 2 Incomplete")
+    
+    L = laplacian(A)
+    
+    eigs,v1 = la.eig(L)
+    
+    eigs = np.real(eigs)
+    connected = 0
+    
+    for i in range(len(eigs)):
+        if eigs[i] < tol:
+            connected += 1
+
+    np.sort(eigs)
+    algcon = eigs[1]
+    
+    return connected, algcon
 
 
 # Helper function for problem 4.
@@ -78,17 +106,60 @@ class ImageSegmenter:
     # Problem 3
     def __init__(self, filename):
         """Read the image file. Store its brightness values as a flat array."""
-        raise NotImplementedError("Problem 3 Incomplete")
+        #Read and scale the matrix then store as an attribute
+        image = imread(filename)
+        scaled = image / 255
+        self.im = scaled
+        self.gray = False
+        
+        if image.ndim == 3:
+            brightness = scaled.mean(axis=2)
+            M,N = brightness.shape
+            flat_brightness = np.ravel(brightness)
+        
+        else:
+            self.gray = True
+            flat_brightness = np.ravel(scaled)
+            
+        self.flatbright = flat_brightness
+        
 
     # Problem 3
     def show_original(self):
         """Display the original image."""
-        raise NotImplementedError("Problem 3 Incomplete")
+        
+        if self.gray == True:
+            plt.imshow(self.im, cmap="gray")
+            plt.axis("off")
+        else:
+            plt.imshow(self.im)
+            plt.axis("off")
+            
+        
 
     # Problem 4
     def adjacency(self, r=5., sigma_B2=.02, sigma_X2=3.):
         """Compute the Adjacency and Degree matrices for the image graph."""
-        raise NotImplementedError("Problem 4 Incomplete")
+        
+        if self.gray == True:
+            m,n = self.im.shape
+            
+        else:
+            m,n,z = self.im.shape
+        
+        A = sp.lil_matrix(m*n,m*n)
+        D = np.zeros([1,m*n])
+        
+        for index in range(m*n):
+            indices, dist = get_neighbors(index, r, m, n)
+            weights = []
+            #print(self.flatbright(index))
+            weights = [(0 - abs(self.flatbright[index] - self.flatbright(ind))/sigma_B2 - dist[i] / sigma_X2) for i, ind in enumerate(indices)]
+            
+            A[index, indices] = weights
+            D[index] = sum(weights)
+        
+        return A, D
 
     # Problem 5
     def cut(self, A, D):
@@ -101,8 +172,12 @@ class ImageSegmenter:
         raise NotImplementedError("Problem 6 Incomplete")
 
 
-# if __name__ == '__main__':
-#     ImageSegmenter("dream_gray.png").segment()
-#     ImageSegmenter("dream.png").segment()
-#     ImageSegmenter("monument_gray.png").segment()
-#     ImageSegmenter("monument.png").segment()
+if __name__ == '__main__':
+    A = np.array([[1,2],[5,0]])
+    print(laplacian(A))
+    print(connectivity(A))
+    ImageSegmenter("dream_gray.png").show_original()
+    ImageSegmenter("dream.png").adjacency()
+#    ImageSegmenter("monument_gray.png").segment()
+#    ImageSegmenter("monument.png").segment()
+    pass
